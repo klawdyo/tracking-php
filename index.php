cd22bdbe3b656341f185eadb144192ca76b80d9e
Claudio Medeiros <klawdyo@users.noreply.github.com>
2018-08-14 13:10:56 -0300

    voltando

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
   $post = ['objetos' => 'DY277947771BR', 'btnPesq' => 'Buscar'];
-
   $ch = curl_init('http://www2.correios.com.br/sistemas/rastreamento/resultado.cfm?');
-
   curl_setopt_array($ch, [
       CURLOPT_RETURNTRANSFER => true,
       CURLOPT_HTTPHEADER => [
@@ -13,48 +11,33 @@
       CURLOPT_ENCODING => '',
       CURLOPT_PROTOCOLS => CURLPROTO_HTTP | CURLPROTO_HTTPS
   ]);
-
   $html = curl_exec($ch);
-
 //  preg_match();
-
 # Create a DOM parser object
 $dom = new DOMDocument();
-
 # Parse the HTML from Google.
 # The @ before the method call suppresses any warnings that
 # loadHTML might throw because of invalid HTML in the page.
 @$dom->loadHTML($html);
-
 $result = [];
 $i = 0;
 # Iterate over all the <a> tags
 foreach($dom->getElementsByTagName('tr') as $row) {
-//   $linha[$i] = parseResult( $row );
-  pr($row);
+  //echo 'nova linha:   ';
+  
+        # Show the <a href>
+//         echo $link->getAttribute('href');
+//     pr($row);
+  $j=0;
+  foreach( $row->getElementsByTagName('td') as $cell ) {
+    $result[$i][($j === 0 ? 'detail' : 'description' )] = $cell->textContent;
+    $j++;
+    //echo 'celula  ' . $cell->textContent . '';
+  }
   $i++;
   
 }
-
-// pr($linha);
-
-
-// function parseResult( $detail ){
-//   $result = [];
-//   $j = 0;
-//   foreach( $row->getElementsByTagName('td') as $cell ) {
-//     $result[($j === 0 ? 'detail' : 'description' )] = $cell->textContent;
-//     $j++;
-//   }
-  
-// //   pr( explode( "\n", $result['description'] ) );
-// //   pr($result);  
-//   return $result;
-  
-}
-
-
-
+pr($result);
 function pr($e){
 echo '<pre>', print_r($e, true), '</pre>';
 }
