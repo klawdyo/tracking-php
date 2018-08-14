f2f967d99a1653fa1322badc7159366d00297da3
Cláudio Medeiros <klawdyo@gmail.com>
2018-08-14 18:09:15 -0300

    Possibilidade de recebimento da função via GET

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
+
+/**
+ * Este arquivo é responsável por pegar as informações da url e realizar a consulta
+ */
+
+// Importa o arquivo
 require './Tracking.php';
 
-$track = new Tracking( 'DY277347772BR' );
-echo $track->asJson();
+// Se o número de rastreamento foi informado na url
+if ( isset( $_GET['tracking_number'] ) ) {
+  // Pega o número do get
+  $trackingNumber = $_GET['tracking_number'];
+  // Gera o resultado
+  $track = new Tracking( $trackingNumber );
+  // Imprime na tela
+  echo $track->asJson();
+} else {
+  echo json_encode( [
+    'lastStatus' => 'Número do Objeto não Informado',
+    'status' => 'invalidTrackingNumber',
+    'isDelivered' => 0,
+    'events' => []
+  ] );
+}
